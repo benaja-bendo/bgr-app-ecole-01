3b80c1b73ac531189c35003bc545bc5d5047d555
benaja-bendo <benaja.bendo02@gmail.com>
2024-01-03 00:22:02 +0100

    [feat] add variable vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_APP_TITLE: string
+    readonly VITE_API_BASE_URL: string
+    readonly VITE_API_TIMEOUT: number
+    readonly VITE_APP_VERSION: string
+    readonly VITE_FACEBOOK_API_KEY: string
+    readonly VITE_GOOGLE_API_KEY: string
+    readonly VITE_API_ROUTE_LOGIN: string
+
+    // more env variables...
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
