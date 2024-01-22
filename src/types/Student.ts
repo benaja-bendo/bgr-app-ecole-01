8c84bc8f11498926e0c504b25f7d8271c5ffb797
benaja-bendo <benaja.bendo02@gmail.com>
2024-01-22 22:47:34 +0100

    #16[✨feat] add studentService

--- src/types/Student.ts
+++ src/types/Student.ts
@@ -3,3 +3,11 @@ import {Tuser} from "@/types/Tuser.ts";
 export type Student = Tuser & {
     speciality?: string;
 }
+
+export type StudentCreateType = {
+    email: string;
+    first_name: string;
+    last_name: string;
+    gender: string;
+    birth_date: string | null;
+}
