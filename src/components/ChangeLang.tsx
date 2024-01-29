3794eb5284cf7e8c0841d7ff3ebb5b9eb38a624b
benaja-bendo <benaja.bendo02@gmail.com>
2024-01-29 11:43:31 +0100

    ✨#38 add react-i18next i18next

---
+++ src/components/ChangeLang.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Button, Menu, MenuItem } from '@mui/material';
+import useChangeLanguage from '@/hooks/use-change-language.ts';
+
+export const ChangeLang = React.forwardRef(() => {
+    const { language, changeLanguage } = useChangeLanguage();
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleChange = (lang: string) => {
+        setAnchorEl(null);
+        changeLanguage(lang);
+    };
+    return (
+        <>
+            <Button
+                id="change-lang"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+                {language}
+            </Button>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleChange}
+                MenuListProps={{
+                    'aria-labelledby': 'change-lang',
+                }}
+            >
+                <MenuItem onClick={() => handleChange('fr')}>Fr</MenuItem>
+                <MenuItem onClick={() => handleChange('en')}>En</MenuItem>
+            </Menu>
+        </>
+    );
+});
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {RouterProvider} from "react-router-dom";
-import {ThemeProvider} from '@mui/material/styles';
-import {CssBaseline} from '@mui/material';
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 
-import {Router} from '@/routes';
-import {createTheme} from '@/theme';
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import { Router } from '@/routes';
+import { createTheme } from '@/theme';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { I18nextProvider } from "react-i18next";
+import i18n from '@/i18n/i18n.ts';
 
 const theme = createTheme();
 const queryClient = new QueryClient();
@@ -15,11 +17,13 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <ReactQueryDevtools initialIsOpen={false}/>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <I18nextProvider i18n={i18n}>
             <ThemeProvider theme={theme}>
-                <CssBaseline/>
-                <RouterProvider router={Router} fallbackElement={<div>Loading...</div>}/>
+                <CssBaseline />
+                <RouterProvider router={Router} fallbackElement={<div>Loading...</div>} />
             </ThemeProvider>
+            </I18nextProvider>
         </QueryClientProvider>
     </React.StrictMode>,
 )
