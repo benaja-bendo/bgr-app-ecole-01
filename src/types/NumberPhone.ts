191804d81edb2e69c3e37318fbcdfadef71736aa
benji_mac <benaja.bendo02@gmail.com>
2024-02-12 17:06:23 +0100

    feat: - add Address type - add NumberPhone type

---
+++ src/types/NumberPhone.ts
@@ -0,0 +1,10 @@
+export type NumberPhone = {
+    id: string;
+    number_phone: string;
+    type: string;
+    is_default: boolean;
+    country_code: string;
+    area_code: string;
+    created_at: string;
+    updated_at: string;
+}
