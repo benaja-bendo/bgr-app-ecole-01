44e938cf8787da97fd03309d4e615f107b6ae943
benji_mac <benaja.bendo02@gmail.com>
2024-03-07 18:04:11 +0100

    - feat DeleteStudent component

--- src/pages/Students/components/tabs/Details.tsx
+++ src/pages/Students/components/tabs/Details.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, FormEvent, useState} from 'react';
 import Grid from '@mui/material/Unstable_Grid2';
 import {
     Button,
@@ -14,6 +14,10 @@ import {
 } from "@mui/material";
 import {useLoaderDataStudent} from "@/hooks/use-loader-data-student.ts";
 import React from 'react';
+import {useTranslation} from "react-i18next";
+import ConfirmDialog from "@/components/ConfirmDialog.tsx";
+import {useFetcher} from "react-router-dom";
+import {DeleteIcon} from "@/components/svg/SvgIcon/DeleteIcon.tsx";
 
 export const Details: FC = () => {
     const student = useLoaderDataStudent();
@@ -113,16 +117,54 @@ export const Details: FC = () => {
                         <Button variant={'outlined'} size="small">Create new payment method</Button>
                     </CardActions>
                 </Card>
-                <Card variant="elevation" elevation={2} square>
-                    <CardHeader title={"Suppression des information personnelles"}/>
-                    <CardContent>
-                        <Button color={'error'} variant={'outlined'} size="small">Delete student</Button>
-                        <Typography variant={'body2'} color={'text.secondary'} sx={{pt:2}}>
-                            Supprimez l'ensemble des informations personnelles de l'étudiant.
-                        </Typography>
-                    </CardContent>
-                </Card>
+                <DeleteStudent/>
             </Stack>
         </Grid>
     </Grid>);
 };
+
+const DeleteStudent: FC = () => {
+    const fetcher = useFetcher();
+    const student = useLoaderDataStudent();
+    const {t} = useTranslation();
+    const [open, setOpen] = useState(false);
+    const [studentToDelete, setStudentToDelete] = useState<number>(0);
+
+    const handleDeleteItem = (e: FormEvent, id: number) => {
+        e.preventDefault();
+        if (id === 0) return
+        setOpen(true);
+        setStudentToDelete(id);
+    }
+    const handleConfirmDelete = async () => {
+        fetcher.submit({ids: studentToDelete}, {
+            action: `/students`,
+            method: 'delete',
+        });
+    }
+    return (<>
+        <ConfirmDialog
+            title="Supprimer l'étudiant"
+            open={open}
+            setOpen={setOpen}
+            onConfirm={handleConfirmDelete}
+        >
+            Êtes-vous sûr de vouloir supprimer cet étudiant ?
+        </ConfirmDialog>
+        <Card variant="elevation" elevation={2} square>
+            <CardHeader title={"Suppression des information personnelles"}/>
+            <CardContent>
+                <Button color={'error'} variant={'outlined'} size="small"
+                        aria-label={t('utils.delete')}
+                        name={"ids"} value={Number(student.id)} type="submit"
+                        onClick={(e) => handleDeleteItem(e, Number(student.id))}
+                        startIcon={<DeleteIcon color={"error"} aria-label={t('utils.delete')}/>}>
+                    Delete student
+                </Button>
+                <Typography variant={'body2'} color={'text.secondary'} sx={{pt:2}}>
+                    Supprimez l'ensemble des informations personnelles de l'étudiant.
+                </Typography>
+            </CardContent>
+        </Card>
+    </>);
+}
