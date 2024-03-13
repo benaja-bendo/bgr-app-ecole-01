4cbce4be14159dfb0d7585118b7626f27d0e62a9
benji_mac <benaja.bendo02@gmail.com>
2024-03-13 18:23:47 +0100

    update DatePicker

--- src/pages/Students/components/DatePicker.tsx
+++ src/pages/Students/components/DatePicker.tsx
@@ -4,8 +4,11 @@ import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 import {DateTimePicker} from '@mui/x-date-pickers/DateTimePicker';
 import dayjs from "dayjs";
 
-export default function DatePicker() {
-    const maxDate = dayjs();
+interface DatePickerProps {
+    defaultValue?: string;
+}
+export default function DatePicker(props: DatePickerProps): JSX.Element {
+    const maxDate = props.defaultValue ? dayjs(props.defaultValue) : dayjs();
     const minDate = dayjs().subtract(100, 'year');
     return (<>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -14,6 +17,7 @@ export default function DatePicker() {
                     name={'birth_date'}
                     maxDate={maxDate}
                     minDate={minDate}
+                    defaultValue={maxDate}
                     views={['year', 'month', 'day']}
                     format={'YYYY-MM-DD'}
                 />
