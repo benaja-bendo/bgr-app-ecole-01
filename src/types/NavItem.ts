bc1db9947b0b42440e0c130f3236186368ba82ec
benji_mac <benaja.bendo02@gmail.com>
2024-06-02 21:27:25 +0200

    Refactor side-nav.tsx to display grouped item list

--- src/types/NavItem.ts
+++ src/types/NavItem.ts
@@ -2,6 +2,7 @@ import React from "react";
 import {RoleType} from "@/types/Role.ts";
 
 export type NavItem = {
+    group?: 'General' | 'User Management' | 'School Management' | 'Settings' | 'Help' | 'Others' | 'Financial'
     title: string;
     path: string;
     icon: React.ReactNode;
