16e6f009afe51451346b42120fd7d0c6f32ad652
benji_mac <benaja.bendo02@gmail.com>
2024-06-24 20:57:16 +0200

    feat

--- src/pages/Agenda/Agenda.tsx
+++ src/pages/Agenda/Agenda.tsx
@@ -3,11 +3,23 @@ import {Schedule} from "@/pages/Students/components/tabs/Schedule.tsx";
 import {Button} from "@mui/material";
 import HttpService from "@/services/HttpService.ts";
 import configRoutes from "@/config/config-routes.ts";
+import FileSaver from "file-saver";
 
 export const Agenda: FC = () => {
     const handleExport = async () => {
-       const req = await HttpService.get(configRoutes.calendarEvents.export(1));
-        // TODO : Export the file (ics or csv)
+        // TODO : Ameloire Export the file (ics or csv)
+        try {
+            const response = await HttpService.get<Blob>(configRoutes.calendarEvents.export(1), {
+                responseType: "blob"
+            });
+            if (response.status === 200) {
+                const filename = 'calendar.ics';
+                FileSaver.saveAs(new Blob([response.data]), filename);
+            }
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     }
     return (<>
         <h1>
